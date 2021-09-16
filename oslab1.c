09a1dc4c5600187294516b8bb4c41b3d04a1a283
abimbaeva <43309818+JulianIsFree@users.noreply.github.com>
2021-09-16 12:42:01 +0700

    review
    no stack alloc

--- oslab1.c
+++ oslab1.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <errno.h>
 #include <pthread.h>
-
-#define LAB_SUCCESS ((void*)0)
-#define LAB_BAD_PARAM ((void*)1)
-#define LAB_FAIL ((void*)2)
+#include <stdlib.h>
+#include <string.h>
 
 #define LAB_THREAD_CREATE_SUCCESS 0
 
+#define LAB_SUCCESS 0
+#define LAB_FAIL 1
+
+typedef struct _lparam {
+    char *str;
+    int count;
+} lparam;
+
 void * run(void * param) {
     if (param == NULL)
-        return (LAB_BAD_PARAM);
-    
-    char *str = (char*)param;
+        return param;
+    lparam *p = (lparam*)param;
+    char *str = p->str;
     int i;
-    for (i=0; i<10; i++) 
+    for (i=0; i<p->count; i++) 
         printf("%d %s\n", i, str);
-	return LAB_SUCCESS;
+	return param;
+}
+
+void print_error(int code, pthread_t thread, char * what) {
+    fprintf(stderr, "Error: %lu %s: %s\n", thread, what, strerror(code));
 }
 
 int main(int argc, char *argv[]) {
@@ -27,22 +37,24 @@ int main(int argc, char *argv[]) {
 
     pthread_t thread;
     pthread_attr_t attr;
+    static lparam child = {"I was  born!", 10};
+    static lparam parent = {"I gave a birth!", 10};
+
     // By calling this we allocate memory for thrattr_t *ap, which is stored in attr->__pthread_attrp,  
     // and has value of *def_thrattr().
     int code = pthread_attr_init(&attr);
-
     if (code == ENOMEM) {
-        fprintf(stderr, "Can't initialise attributes");
-        pthread_exit(LAB_FAIL);
+        print_error(code, thread, "can't init attr");
+        exit(LAB_FAIL);
     }
 
-    code = pthread_create(&thread, &attr, run, "I was born!");
+    code = pthread_create(&thread, &attr, run, &child);
 
     if  (code != LAB_THREAD_CREATE_SUCCESS) {
-        fprintf(stderr, "Can't create thread");
-        pthread_exit(LAB_FAIL);
+        print_error(code, thread, "can't create thread");
+        exit(LAB_FAIL);
     }
-    run("I gave a birth!");
+    run(&parent);
 
     // By calling this we free attr->__pthread_attrp and set attr->__pthread_attrp to NULL
     pthread_attr_destroy(&attr);
