3787cf2a4f6c11faa1c1060f9a2ff74b8b25707c
abimbaeva <43309818+JulianIsFree@users.noreply.github.com>
2021-09-16 11:39:23 +0700

    review
    1 and 2 labs

--- oslab2.c
+++ oslab2.c
@@ -1,48 +1,68 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <errno.h>
 #include <pthread.h>
-#include <string.h>
+
+#define LAB_SUCCESS ((void*)0)
+#define LAB_BAD_PARAM ((void*)1)
+#define LAB_FAIL ((void*)2)
+
+#define LAB_THREAD_CREATE_SUCCESS 0
 
 void * run(void * param) {
+    if (param == NULL)
+        return (LAB_BAD_PARAM);
+    
+    char *str = (char*)param;
     int i;
-    for(i=0; i<10; i++) 
-        printf("I was born!\n");
-	return NULL;
+    for (i=0; i<10; i++) 
+        printf("%d %s\n", i, str);
+	return LAB_SUCCESS;
 }
 
 int main(int argc, char *argv[]) {
     // since @param attr is null, default attributes for child are set up using pthread_attr_init(3C)
-    // https://illumos.org/man/3C/pthread_create
-    // https://illumos.org/man/3C/pthread_attr_init
+    // https://illumos.org/man/3C/pthread_create usr/src/lib/libc/port/threads/pthread.c
+    // https://illumos.org/man/3C/pthread_attr_init usr/src/lib/libc/port/threads/pthr_attr.c
+    // https://illumos.org/man/3C/pthread_exit usr/src/lib/libc/port/threads/thr.c
 
     pthread_t thread;
-    // next 3 lines of code are equal to: 
-    // int code = pthread_create(&thread, NULL, run, NULL);
     pthread_attr_t attr;
-    pthread_attr_init(&attr); 
-    int code = pthread_create(&thread, &attr, run, NULL);
-    if (code!=0) {
-        char buf[256];
-        strerror_r(code, buf, sizeof buf);
-        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
-        exit(1);
+    // By calling this we allocate memory for thrattr_t *ap, which is stored in attr->__pthread_attrp,  
+    // and has value of *def_thrattr().
+    int code = pthread_attr_init(&attr);
+
+    if (code == ENOMEM) {
+        fprintf(stderr, "Can't initialise attributes");
+        pthread_exit(LAB_FAIL);
     }
 
-    void * result;
-    code = pthread_join(thread, &result);
-    if (code!=0) {
-        char buf[256];
-        strerror_r(code, buf, sizeof buf);
-        fprintf(stderr, "%s: creating thread: %s\n", argv[0], buf);
-        exit(1);
+    code = pthread_create(&thread, &attr, run, "I was born!");
+
+    if  (code != LAB_THREAD_CREATE_SUCCESS) {
+        fprintf(stderr, "Can't create thread");
+        pthread_exit(LAB_FAIL);
     }
-    
-    for(int i=0; i<10; i++) 
-        printf("I gave a birth!\n");
-    
-    // https://illumos.org/man/3C/pthread_attr_init
+
+    int *status;
+    pthread_join(thread, (void**)(&status));
+    if (status == LAB_BAD_PARAM) {
+        fprintf(stderr, "Bad params for child thread");
+        pthread_exit(LAB_FAIL);
+    } 
+
+    if (status != LAB_SUCCESS) {
+        fprintf(stderr, "Unknown status: %d", status);
+    }
+
+    run("I gave a birth!");
+
+    // By calling this we lfree attr->__pthread_attrp and set attr->__pthread_attrp to NULL
     pthread_attr_destroy(&attr);
-    // https://illumos.org/man/3C/pthread_exit
-    pthread_exit(NULL);  
-    return 0;
+    /*
+    from https://illumos.org/man/3C/pthread_exit: 
+    The process exits with an exit status of  0 after the last thread has
+    been terminated. The behavior is as if the implementation called exit()
+    with a 0 argument at thread termination time.
+    */
+    pthread_exit(LAB_SUCCESS);  
 }
