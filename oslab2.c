17e7c928ea26fec90954de12d49c069389438a8b
abimbaeva <43309818+JulianIsFree@users.noreply.github.com>
2021-09-23 01:07:02 +0700

    lab2 update
    with answers for questions
    
    remember detached against joinable

--- oslab2.c
+++ oslab2.c
@@ -7,6 +7,7 @@
 #define LAB_FAIL ((void*)2)
 
 #define LAB_THREAD_CREATE_SUCCESS 0
+#define LAB_THREAD_JOIN_SUCCESS 0
 
 void * run(void * param) {
     if (param == NULL)
@@ -19,36 +20,63 @@ void * run(void * param) {
 	return LAB_SUCCESS;
 }
 
-int main(int argc, char *argv[]) {
-    // since @param attr is null, default attributes for child are set up using pthread_attr_init(3C)
-    // https://illumos.org/man/3C/pthread_create usr/src/lib/libc/port/threads/pthread.c
-    // https://illumos.org/man/3C/pthread_attr_init usr/src/lib/libc/port/threads/pthr_attr.c
-    // https://illumos.org/man/3C/pthread_exit usr/src/lib/libc/port/threads/thr.c
+void print_error(int code, pthread_t thread, char * what) {
+    fprintf(stderr, "Error: %lu %s: %s\n", thread, what, strerror(code));
+}
 
+int main(int argc, char *argv[]) {
     pthread_t thread;
     pthread_attr_t attr;
-    // By calling this we allocate memory for thrattr_t *ap, which is stored in attr->__pthread_attrp,  
-    // and has value of *def_thrattr().
-    int code = pthread_attr_init(&attr);
 
+    // read oslab1.c for comments
+    int code = pthread_attr_init(&attr);
     if (code == ENOMEM) {
         fprintf(stderr, "Can't initialise attributes");
         pthread_exit(LAB_FAIL);
     }
 
     code = pthread_create(&thread, &attr, run, "I was born!");
-
+    
     if  (code != LAB_THREAD_CREATE_SUCCESS) {
-        fprintf(stderr, "Can't create thread");
+        print_error(code, thread, "can't create thread");
         pthread_exit(LAB_FAIL);
     }
 
     int *status;
-    pthread_join(thread, (void**)(&status));
-    if (status == LAB_BAD_PARAM) {
-        fprintf(stderr, "Bad params for child thread");
+    // https://illumos.org/man/3C/pthread_join usr/src/lib/libc/port/threads/thr.c
+    // https://illumos.org/man/3C/pthread_detach usr/src/lib/libc/port/threads/thr.c
+    code = pthread_join(thread, (void**)(&status));
+    if (code == EINVAL) {
+        print_error(code, thread, "target thread is detached");
         pthread_exit(LAB_FAIL);
     } 
+    if (code == ESRCH) {
+        print_error(code, thread, "someone stole your sweet role");
+        pthread_exit(LAB_FAIL);
+    }
+    // ? handling
+    // ? join to detached
+    // ? multi join
+    // ? what is zombie and what stored
+    if (status == LAB_BAD_PARAM) {
+        print_error(code, thread, "bad params for joined thread");
+        pthread_exit(LAB_FAIL);
+    } 
+    /*
+    What is freed:
+        in structure ulwp_t for this thread there is a uberdata structure, in which there are several things:
+            tmem list
+            thread specific data list 
+            thread local storage 
+            heldlock list
+            :: all of these are deallocated (except heldlock list, for they owner is set as LOCK_OWNERDEAD)
+        ulwp_t->ul_readlock.array is freed with lfree (rwl_free(ulwp_t*));
+        ulwp_t structure after this is moved either in free stack list or free ulwp_t list (ulwp_free(ulwp_t*) function)
+
+    Zombie thread is a thread which already called pthread_exit but which rval were not gained by any other thread
+    Trying to join to detached will result in EINVAL
+    Multi join will result in situation, where one thread obtains result, and others receives ESRCH
+    */
 
     if (status != LAB_SUCCESS) {
         fprintf(stderr, "Unknown status: %d", status);
@@ -56,13 +84,7 @@ int main(int argc, char *argv[]) {
 
     run("I gave a birth!");
 
-    // By calling this we lfree attr->__pthread_attrp and set attr->__pthread_attrp to NULL
+    // read oslab1.c for comments
     pthread_attr_destroy(&attr);
-    /*
-    from https://illumos.org/man/3C/pthread_exit: 
-    The process exits with an exit status of  0 after the last thread has
-    been terminated. The behavior is as if the implementation called exit()
-    with a 0 argument at thread termination time.
-    */
     pthread_exit(LAB_SUCCESS);  
 }
